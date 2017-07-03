f0426c28586ed207e3fd67e0b8a4ed116c83ae79
InnovateBham_13 <InnovateBham_13@Admins-iMac.local>
2017-07-03 10:22:07 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+var friends = ["Candace", "Kelcey", "Brian", "Mike", "Jeffery"]
+var location = ["i lab", "bathroom", "gen it classroom", "parking deck", "pool", 
+"bedroom", "graveyard", "car lot", "ice cream room", "sanctuary"]
+var weapons = ["knife", "bazooka", "trump vote", "galactic gun", "thunderbolt", "bad meme", 
+"spanking", "covalence lab", "jquery function", "mage bomb", "arcane shot", "trap card", 
+"iphone charger", "samsung battery", "bad driver", "array function", "sharp candy cane",
+"mountain dew", "master baiter", "denial of basic human rights"]
+
+
+});
+
+
+
