485e35079f3ffecbc874be608206c877f5189271
InnovateBham_13 <InnovateBham_13@Admins-iMac.local>
2017-07-05 15:20:50 -0700

    final push

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+var friends = ["Candace", "Adrian", "Brian", "JB", "Greg"]
+var locations = ["i lab", "bathroom", "gen it classroom", "parking deck", "pool", "bedroom", "graveyard", "car lot", "ice cream room", "sanctuary"]
+var weapons = ["healthcare repeal.", "bazooka.", "trump vote.", "galactic gun.", "thunderbolt.", "bad meme.", "hyper beam.", "covalence lab.", "jquery function.", "mage bomb.", "arcane shot.", "trap card.", "iphone charger.", "samsung battery.", "bad driver.", "array function.", "sharp candy cane.", "mountain dew.", "master baiter.", "denial of basic human rights."];
+
 document.addEventListener("DOMContentLoaded", function() {
 
-var friends = ["Candace", "Kelcey", "Brian", "Mike", "Jeffery"]
-var location = ["i lab", "bathroom", "gen it classroom", "parking deck", "pool", 
-"bedroom", "graveyard", "car lot", "ice cream room", "sanctuary"]
-var weapons = ["knife", "bazooka", "trump vote", "galactic gun", "thunderbolt", "bad meme", 
-"spanking", "covalence lab", "jquery function", "mage bomb", "arcane shot", "trap card", 
-"iphone charger", "samsung battery", "bad driver", "array function", "sharp candy cane",
-"mountain dew", "master baiter", "denial of basic human rights"]
+for(var i = 1; i <= 100; i++) {
+    var tag = document.createElement('h3');
+    tag.innerText = 'Clue ' + i;  
+    var set = {
+        num: i,
+        friend: friends[Math.floor(Math.random() * friends.length)],
+        location: locations[Math.floor(Math.random() * locations.length)],
+        weapon: weapons[Math.floor(Math.random() * weapons.length)]
+};
+  
+  tag.addEventListener('click', config.bind(set));
+  document.body.appendChild(tag);
 
-
+    }
 });
+
+function config() {
+    alert(this.friend + ' did it in the ' + this.location + ' with a ' + this.weapon);
+}
+
+
+
 
 
 
